dce2c83a22f29a4af9dbd34b193db36a7e088582
Martin Paul <mortib20@gmail.com>
2025-11-20 19:41:20 +0100

    actual fix

---
+++ Mesher.Database/Migrations/20251120184023_FixForNullOnSomeFields.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mesher.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixForNullOnSomeFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "SNR",
+                table: "MeshMessages",
+                type: "double precision",
+                nullable: true,
+                computedColumnSql: "(CAST(\"RawMessage\" ->> 'snr' AS double precision))",
+                stored: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldComputedColumnSql: "(CAST(\"RawMessage\" ->> 'snr' AS double precision))",
+                oldStored: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RSSI",
+                table: "MeshMessages",
+                type: "double precision",
+                nullable: true,
+                computedColumnSql: "(CAST(\"RawMessage\" ->> 'rssi' AS double precision))",
+                stored: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldComputedColumnSql: "(CAST(\"RawMessage\" ->> 'rssi' AS double precision))",
+                oldStored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "SNR",
+                table: "MeshMessages",
+                type: "double precision",
+                nullable: false,
+                computedColumnSql: "(CAST(\"RawMessage\" ->> 'snr' AS double precision))",
+                stored: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true,
+                oldComputedColumnSql: "(CAST(\"RawMessage\" ->> 'snr' AS double precision))",
+                oldStored: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "RSSI",
+                table: "MeshMessages",
+                type: "double precision",
+                nullable: false,
+                computedColumnSql: "(CAST(\"RawMessage\" ->> 'rssi' AS double precision))",
+                stored: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true,
+                oldComputedColumnSql: "(CAST(\"RawMessage\" ->> 'rssi' AS double precision))",
+                oldStored: true);
+        }
+    }
+}
