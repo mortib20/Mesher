10b6c1047a5738b2e88ca82423ed5f8cee31b5a1
Martin Paul <mortib20@gmail.com>
2025-12-07 18:54:24 +0100

    options

---
+++ Mesher.Mesh/Config/MeshServiceMQTTConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mesher.Mesh.Config;
+
+// ReSharper disable once InconsistentNaming
+public class MeshServiceMQTTConfig
+{
+    public static string Section = "MeshServiceMQTT";
+    [Required]
+    public required string Server { get; init; }
+    [Required]
+    public required string Topic { get; set; }
+}
--- Mesher.Mesh/MeshServiceMQTT.cs
+++ Mesher.Mesh/MeshServiceMQTT.cs
@@ -1,32 +1,37 @@
 ﻿using System.Text;
 using Mesher.Database;
+using Mesher.Mesh.Config;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MQTTnet;
 
 namespace Mesher.Mesh;
 
 // ReSharper disable once InconsistentNaming
-public class MeshServiceMQTT(ILogger<MeshServiceMQTT> logger, IServiceProvider serviceProvider) : Global.Service.AviatorBackgroundService(logger)
+public class MeshServiceMQTT(ILogger<MeshServiceMQTT> logger, IOptions<MeshServiceMQTTConfig> configInjected, IServiceProvider serviceProvider) : Global.Service.AviatorBackgroundService(logger)
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var config = configInjected.Value; 
         var mqttFactory = new MqttClientFactory();
 
         using var mqttClient = mqttFactory.CreateMqttClient();
-        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("192.168.168.3").Build();
+        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(config.Server).Build();
+        
+        logger.LogInformation("Connected to {Server} MQTT-Server.", config.Server);
 
         mqttClient.ApplicationMessageReceivedAsync += async e =>
         {
             await using var serviceScope = serviceProvider.CreateAsyncScope();
             await using var dbContext = serviceScope.ServiceProvider.GetRequiredService<MesherContext>();
-            
+
             logger.LogInformation("Received application message");
             var mqttPayload = e.ApplicationMessage.Payload;
             var meshJson = Encoding.UTF8.GetString(mqttPayload);
             // var meshPayload = JsonSerializer.Deserialize<MeshPayload>(mqttPayload.ToArray());
 
-            var entry = new DbMeshMessage()
+            var entry = new DbMeshMessage
             {
                 RawMessage = meshJson
             };
@@ -35,14 +40,14 @@ public class MeshServiceMQTT(ILogger<MeshServiceMQTT> logger, IServiceProvider s
             await dbContext.MeshMessages.AddAsync(entry, stoppingToken);
             await dbContext.SaveChangesAsync(stoppingToken);
         };
-        
+
         await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
 
-        var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter("msh/2/json/#").Build();
+        var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(config.Topic).Build();
 
         await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
-        
-        logger.LogInformation("MQTT client subscribed to topic.");
+
+        logger.LogInformation("MQTT client subscribed to {topic} topic.", config.Topic);
 
         while (!stoppingToken.IsCancellationRequested)
         {
