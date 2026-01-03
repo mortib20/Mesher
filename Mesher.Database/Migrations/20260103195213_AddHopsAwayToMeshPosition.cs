8b830206ad94e4cc382254529866e22b51ef1506
Martin Paul <martin.paul-3277@web.de>
2026-01-03 21:01:19 +0100

    added hopsaway to MeshPositions

---
+++ Mesher.Database/Migrations/20260103195213_AddHopsAwayToMeshPosition.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mesher.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHopsAwayToMeshPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR REPLACE VIEW ""MeshPosition"" AS
+SELECT ""From"",
+       (""RawMessage"" -> 'payload' -> 'latitude_i')::double precision / 10000000  AS ""Latitude"",
+       (""RawMessage"" -> 'payload' -> 'longitude_i')::double precision / 10000000 AS ""Longitude"",
+       (""RawMessage"" -> 'payload' -> 'altitude')::bigint                         AS ""Altitude"",
+       (""RawMessage"" -> 'payload' -> 'precision_bits')::bigint                   AS ""PrecisionBits"",
+       to_timestamp((""RawMessage"" -> 'payload' -> 'time')::bigint)               AS ""Time"",
+       ""CreatedAt"",
+       ""HopsAway""
+FROM ""MeshMessages""
+WHERE ""Type"" = 'position';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
