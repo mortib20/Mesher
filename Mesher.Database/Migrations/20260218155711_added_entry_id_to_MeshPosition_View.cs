46740f871b28cbe344130e9a87852c55642166b1
Martin Paul <mortib20@gmail.com>
2026-02-18 17:04:05 +0100

    add entryId to meshposition

---
+++ Mesher.Database/Migrations/20260218155711_added_entry_id_to_MeshPosition_View.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mesher.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_entry_id_to_MeshPosition_View : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE OR REPLACE VIEW ""MeshPosition"" AS
+SELECT ""From"",
+       (""RawMessage"" -> 'payload' -> 'latitude_i')::double precision / 10000000  AS ""Latitude"",
+       (""RawMessage"" -> 'payload' -> 'longitude_i')::double precision / 10000000 AS ""Longitude"",
+       (""RawMessage"" -> 'payload' -> 'altitude')::bigint                         AS ""Altitude"",
+       (""RawMessage"" -> 'payload' -> 'precision_bits')::bigint                   AS ""PrecisionBits"",
+       to_timestamp((""RawMessage"" -> 'payload' -> 'time')::bigint)               AS ""Time"",
+       ""CreatedAt"",
+       ""HopsAway"",
+       ""EntryId""
+FROM ""MeshMessages""
+WHERE ""Type"" = 'position';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
